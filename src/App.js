1d117ffbf3db666a9d47dcdf4fdec66279919e75
SilasCundiff <silinski23@gmail.com>
2021-05-17 17:14:42 -0400

    mobile nav and hero completed, main and footer started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import ReturnToTop from './components/independent/ReturnToTop/ReturnToTop';
+import Header from './components/header/Header.js';
+import Main from './components/main/Main.js';
+import './scss/global.scss';
+import Footer from './components/footer/Footer';
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+      <ReturnToTop />
+    </div>
+  );
 }
 
 export default App;
