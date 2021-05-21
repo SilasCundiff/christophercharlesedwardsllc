5d557a0fd9ae5c11adadf73c98467a1d15dd7867
SilasCundiff <silinski23@gmail.com>
2021-05-21 16:13:12 -0400

    background performence testing

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContactButton from '../independent/ContactButton/ContactButton';
 
+import PatternDivider from '../independent/Dividers/PatternDivider/PatternDivider';
 import FirstArticle from './FirstArticle/FirstArticle';
 import ListOfCases from './ListOfCases/ListOfCases';
 import './Main.scss';
@@ -8,7 +9,7 @@ const Main = ({ handleClickModal }) => {
   return (
     <main className='Main'>
       <FirstArticle />
-
+      <PatternDivider />
       <ListOfCases />
       <ContactButton handleClickModal={handleClickModal} />
     </main>
